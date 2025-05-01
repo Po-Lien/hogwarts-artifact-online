93519f37814ddd4d9bc0398820db334701caa4af
Po-Lien <>
2025-05-01 15:57:16 +0800

    User CRUD done! Close #4

---
+++ src/main/java/idv/ex/cs/hogwartsartifactsonline/hogwartsuser/converter/UserDtoToUserConverter.java
@@ -0,0 +1,20 @@
+package idv.ex.cs.hogwartsartifactsonline.hogwartsuser.converter;
+
+import idv.ex.cs.hogwartsartifactsonline.hogwartsuser.HogwartsUser;
+import idv.ex.cs.hogwartsartifactsonline.hogwartsuser.dto.UserDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoToUserConverter implements Converter<UserDto, HogwartsUser> {
+
+    @Override
+    public HogwartsUser convert(UserDto source) {
+        HogwartsUser hogwartsUser = new HogwartsUser();
+        hogwartsUser.setUsername(source.username());
+        hogwartsUser.setEnabled(source.enabled());
+        hogwartsUser.setRoles(source.roles());
+        return hogwartsUser;
+    }
+
+}
--- src/main/java/idv/ex/cs/hogwartsartifactsonline/wizard/Wizard.java
+++ src/main/java/idv/ex/cs/hogwartsartifactsonline/wizard/Wizard.java
@@ -3,16 +3,12 @@ package idv.ex.cs.hogwartsartifactsonline.wizard;
 import idv.ex.cs.hogwartsartifactsonline.artifact.Artifact;
 import jakarta.persistence.*;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Wizard implements Serializable {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
